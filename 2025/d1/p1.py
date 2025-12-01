e63a9837becd774e45326303ad2a8b321e4772e7
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-12-01 10:58:01 -0500

    horrible no good 2025d1, i'm not editing this into anything better my shame will be public for all to see

---
+++ 2025/d1/p1.py
@@ -0,0 +1,21 @@
+with open('input.txt') as f:
+    lines = [l.strip() for l in f.readlines()]
+
+current, count1, count2 = 50, 0, 0
+for l in lines:
+    dir, num = 1 if l[0] == 'R' else -1, int(l[1:])
+    curr_tmp = current + dir * num
+
+    # holy f i'm ashamed of this
+    hundreds_digit = curr_tmp // 100
+    zero_count = abs(hundreds_digit)
+    zero_count += (curr_tmp % 100 == 0 if hundreds_digit < 0 else curr_tmp == 0)
+    zero_count -= (current == 0 and hundreds_digit < 0)
+
+    print(f'line = {l}, current = {current}, new = {curr_tmp}, added = {zero_count}')
+    current = curr_tmp % 100
+    count1 += (current == 0)
+    count2 += zero_count
+
+print(count1)
+print(count2)
